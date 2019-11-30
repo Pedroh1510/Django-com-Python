ba9d739a50601114757772c10bdf51bb1c165d05
Pedro <pedroh1510@hotmail.com>
2019-11-30 10:56:24 -0300

    Cria pagina que retorne informações do evento

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Eventos
 
 # Create your views here.
+def eventoLocal(requests,evento):
+    eventoObject = Eventos.objects.get(titulo=evento)
+    page= []
+    page.append(f'<li>Descrição do evento: {eventoObject.usuario}</li>')
+    page.append(f'<li>Descrição do evento: {eventoObject.descricao}</li>')
+    page.append(f'<li>Data do evento: {eventoObject.dataEvento}</li>')
+    page.append(f'<li>Local do evento: {eventoObject.local}</li>')
+    return HttpResponse(page)
